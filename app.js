bc0285e06c133338ee10d5eb0a1bf60219cf12d1
Joel Rodas Sánchez <joel.rodas.sanchez@gmail.com>
2023-07-13 14:34:18 -0500

    reparo

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var archivo = document.getElementById("desgraduacion");
 archivo.addEventListener("click", function(event) {
   event.preventDefault(); 
-  var url = "https://docs.google.com/spreadsheets/d/1HnogfYg20gTJKZoA_CnPBM7-X_ahIG7as-cbdD0XEIg/export?usp=sharing";
+  let url = "https://docs.google.com/spreadsheets/d/1HnogfYg20gTJKZoA_CnPBM7-X_ahIG7as-cbdD0XEIg/export?usp=sharing";
   window.open(url, "_blank");
 });
 
-var archivo = document.getElementById("inscripcion");
-archivo.addEventListener("click", function(event) {
+var archivo2 = document.getElementById("inscripcion");
+archivo2.addEventListener("click", function(event) {
   event.preventDefault();
-  var url = "https://docs.google.com/spreadsheets/d/10lxVg-5KBFKlFdWgn18QSJOrFviT9PbTgZMD6DhYdKQ/edit?usp=sharing";
+  let url = "https://docs.google.com/spreadsheets/d/10lxVg-5KBFKlFdWgn18QSJOrFviT9PbTgZMD6DhYdKQ/edit?usp=sharing";
   window.open(url, "_blank");
 });
 
